8258074b57aabe3cfa013f882a2a1fcf9701474b
Lucas Santana <lucas.diassantana@gmail.com>
2023-06-09 00:50:16 -0300

    changes to database

---
+++ server/prisma/migrations/20230609015354_changed/migration.sql
@@ -0,0 +1,50 @@
+/*
+  Warnings:
+
+  - The primary key for the `ContentList` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `ContentType` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `ContentStatus` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Genres` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_ContentList" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "content_status" TEXT NOT NULL,
+    "global_rating" REAL NOT NULL DEFAULT 0,
+    "personal_rating" REAL DEFAULT 0,
+    "genres" TEXT NOT NULL,
+    "images" TEXT NOT NULL,
+    "creation_timestamp" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "ContentList_content_status_fkey" FOREIGN KEY ("content_status") REFERENCES "ContentStatus" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_ContentList" ("content_status", "creation_timestamp", "genres", "global_rating", "id", "images", "name", "personal_rating", "type") SELECT "content_status", "creation_timestamp", "genres", "global_rating", "id", "images", "name", "personal_rating", "type" FROM "ContentList";
+DROP TABLE "ContentList";
+ALTER TABLE "new_ContentList" RENAME TO "ContentList";
+CREATE TABLE "new_ContentType" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
+INSERT INTO "new_ContentType" ("id", "name") SELECT "id", "name" FROM "ContentType";
+DROP TABLE "ContentType";
+ALTER TABLE "new_ContentType" RENAME TO "ContentType";
+CREATE TABLE "new_ContentStatus" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "status" TEXT NOT NULL,
+    "creation_timestamp" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_ContentStatus" ("creation_timestamp", "id", "status") SELECT "creation_timestamp", "id", "status" FROM "ContentStatus";
+DROP TABLE "ContentStatus";
+ALTER TABLE "new_ContentStatus" RENAME TO "ContentStatus";
+CREATE TABLE "new_Genres" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
+INSERT INTO "new_Genres" ("id", "name") SELECT "id", "name" FROM "Genres";
+DROP TABLE "Genres";
+ALTER TABLE "new_Genres" RENAME TO "Genres";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- server/src/server/routes.ts
+++ server/src/server/routes.ts
@@ -1,20 +1,56 @@
 import { Router, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { randomUUID } from "crypto";
 
 const router = Router();
 
 const prisma = new PrismaClient();
 
-router.get("/", (req: Request, res: Response) => {
-	res.json({ message: "Teste" });
+router.get("/animes/:apiKey", async (req: Request, res: Response) => {
+	const apiKey = req.params.apiKey;
+	let data: {
+		genres: Array<{
+			id: number;
+			name: string;
+		}>;
+	};
+	await fetch("https://api.themoviedb.org/3/genre/movie/list", {
+		method: "GET",
+		credentials: "include",
+		headers: {
+			Authorization: `Bearer ${apiKey}`,
+			"Content-Type": "application/json",
+		},
+	})
+		.then((response) => response.json())
+		.then((res) => {
+			data = res;
+		});
+
+	const genres = data!.genres;
+
+	genres.forEach(
+		async (genre: { id: number; name: string }) =>
+			await prisma.genres.create({
+				data: {
+					id: randomUUID(),
+					name: genre.name,
+					content_type: 'Movie'
+				},
+			})
+	);
+
+	return res.status(200);
 });
 
-router.get("/animes", async (req: Request, res: Response) => {
-	const animes = await prisma.contentList.findMany({
-		where: { name: "Os Mimios" },
-	});
+// router.post("/watchlist", async (req: Request, res: Response) => {
+// 	const animes = await prisma.contentList.create({
+// 		data: {
 
-	return res.status(200).json(animes);
-});
+// 		}
+// 	});
+
+// 	return res.status(200).json(animes);
+// });
 
 export default router;
