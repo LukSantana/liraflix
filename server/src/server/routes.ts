012519da0785347acde9602737ad59c2987209d7
Lucas Santana Dias <lucas.santana@trinusco.com.br>
2023-06-09 22:03:15 -0300

    created update content status route

--- server/src/server/routes.ts
+++ server/src/server/routes.ts
@@ -6,84 +6,132 @@ const router = Router();
 
 const prisma = new PrismaClient();
 
-router.get("/contentlist", async (req: Request, res: Response) => {
-	let contentId = req.query.id;
-	let contentName = req.query.name;
-	let contentStatus = req.query.status;
-	let contentType = req.query.type;
+router.get("/content/list", async (req: Request, res: Response) => {
+	try {
+		let contentId = req.query.id;
+		let contentName = req.query.name;
+		let contentStatus = req.query.status;
+		let contentType = req.query.type;
 
-	let whereProps: {
-		id: any;
-		name: any;
-		content_status: any;
-		content_type: any;
-	} = {
-		id: contentId,
-		name: contentName,
-		content_status: contentStatus,
-		content_type: contentType,
-	};
+		let whereProps: {
+			id: any;
+			name: any;
+			content_status: any;
+			content_type: any;
+		} = {
+			id: contentId,
+			name: contentName,
+			content_status: contentStatus,
+			content_type: contentType,
+		};
 
-	let response;
+		let response;
 
-	response = await prisma.contentList.findMany({
-		where: whereProps,
-	});
+		response = await prisma.contentList.findMany({
+			where: whereProps,
+		});
 
-	return res.status(200).json(response);
+		return res.status(200).json(response);
+	} catch (e: any | undefined) {
+		throw new Error(e.message);
+	}
 });
 
 router.post("/content", async (req: Request, res: Response) => {
-	const body = req.body;
+	try {
+		const body = req.body;
 
-	const contentName = body.name;
+		const contentName = body.name;
 
-	const contentStatusResponse = await prisma.contentStatus.findFirst({
-		where: {
-			status: body.content_status,
-		},
-	});
-	const contentStatus = contentStatusResponse?.id
+		const contentStatusResponse = await prisma.contentStatus.findFirst({
+			where: {
+				status: body.content_status,
+			},
+		});
+		const contentStatus = contentStatusResponse?.id;
 
-	const contentTypeResponse = await prisma.contentType.findFirst({
-		where: {
-			name: body.content_type,
-		},
-	});
-	const contentType = contentTypeResponse?.id
+		const contentTypeResponse = await prisma.contentType.findFirst({
+			where: {
+				name: body.content_type,
+			},
+		});
+		const contentType = contentTypeResponse?.id;
 
-	const globalRating = body.global_rating;
-	const personalRating = body.personal_rating;
-	const genres = body.genres;
-	const images = body.images;
+		const globalRating = body.global_rating;
+		const personalRating = body.personal_rating;
+		const genres = body.genres;
+		const images = body.images;
 
-	const queryData: {
-		id: any;
-		name: any;
-		content_status: any;
-		content_type: any;
-		global_rating: any;
-		personal_rating: any;
-		genres: any;
-		images: any;
-	} = {
-		id: randomUUID(),
-		name: contentName,
-		content_status: contentStatus,
-		content_type: contentType,
-		global_rating: globalRating,
-		personal_rating: personalRating && personalRating,
-		genres: genres,
-		images: images,
-	};
+		const queryData: {
+			id: any;
+			name: any;
+			content_status: any;
+			content_type: any;
+			global_rating: any;
+			personal_rating: any;
+			genres: any;
+			images: any;
+		} = {
+			id: randomUUID(),
+			name: contentName,
+			content_status: contentStatus,
+			content_type: contentType,
+			global_rating: globalRating,
+			personal_rating: personalRating && personalRating,
+			genres: genres,
+			images: images,
+		};
 
-	console.log(queryData);
+		const response = await prisma.contentList.create({
+			data: queryData,
+		});
 
-	const response = await prisma.contentList.create({
-		data: queryData,
-	});
+		return res.status(200).json(response);
+	} catch (e: any | undefined) {
+		throw new Error(e);
+	}
+});
 
-	return res.status(200).json(response);
+router.get("/content/:id", async (req: Request, res: Response) => {
+	try {
+		const contentId = req.params.id;
+
+		const response = await prisma.contentList.findFirst({
+			where: {
+				id: contentId,
+			},
+		});
+
+		return res.status(200).json(response);
+	} catch (e: any | undefined) {
+		throw new Error(e.message);
+	}
+});
+
+router.put("/content/:id", async (req: Request, res: Response) => {
+	try {
+		const contentId = req.params.id;
+		const contentStatus = req.body.content_status;
+
+		const contentStatusResponse = await prisma.contentStatus.findFirst({
+			where: {
+				status: contentStatus,
+			},
+		});
+
+
+
+		const response = await prisma.contentList.update({
+			where: { id: contentId },
+			data: {
+				content_status: contentStatusResponse?.id
+			}
+		});
+
+		return res.status(200).json(response);
+	} catch (e: any | undefined) {
+		throw new Error(e.message);
+	}
 });
 
 export default router;
