467ed25998e95dfb3295dd51c8c38f35cebd7241
Lucas Santana <lucas.diassantana@gmail.com>
2023-06-15 16:41:37 -0300

    changes to format

--- web/src/utils/routesInfo.tsx
+++ web/src/utils/routesInfo.tsx
@@ -5,53 +5,53 @@ import Search from "../pages/Search";
 import Watchlist from "../pages/Watchlist";
 
 interface RoutesProps {
-  routeName: string;
-  routeDescription: string;
-  path: string;
-  element: JSX.Element;
+	routeName: string;
+	routeDescription: string;
+	path: string;
+	element: JSX.Element;
 }
 
 class routesInfo {
-  basicRoutes: Array<RoutesProps>;
-  dynamicRoutes: Array<RoutesProps>;
+	basicRoutes: Array<RoutesProps>;
+	dynamicRoutes: Array<RoutesProps>;
 
-  constructor() {
-    this.basicRoutes = [
-      {
-        routeName: "Home",
-        routeDescription: "Home Page",
-        path: "/",
-        element: <Home />,
-      },
-      {
-        routeName: "Minha Lista",
-        routeDescription: "Minha lista de filmes e animes",
-        path: "/watchlist",
-        element: <Watchlist />,
-      },
-    ];
+	constructor() {
+		this.basicRoutes = [
+			{
+				routeName: "Home",
+				routeDescription: "Home Page",
+				path: "/",
+				element: <Home />,
+			},
+			{
+				routeName: "Minha Lista",
+				routeDescription: "Minha lista de filmes e animes",
+				path: "/watchlist",
+				element: <Watchlist />,
+			},
+		];
 
-    this.dynamicRoutes = [
-      {
-        routeName: "Filmes",
-        routeDescription: "Página de Filmes",
-        path: "/movies",
-        element: <Movies />,
-      },
-      {
-        routeName: "Animes",
-        routeDescription: "Página de Animes",
-        path: "/animes",
-        element: <Animes />,
-      },
-      {
-        routeName: "Search",
-        routeDescription: "Página de busca de conteúdo",
-        path: "/search",
-        element: <Search/>,
-      },
-    ];
-  }
+		this.dynamicRoutes = [
+			{
+				routeName: "Filmes",
+				routeDescription: "Página de Filmes",
+				path: "/movies",
+				element: <Movies />,
+			},
+			{
+				routeName: "Animes",
+				routeDescription: "Página de Animes",
+				path: "/animes",
+				element: <Animes />,
+			},
+			{
+				routeName: "Search",
+				routeDescription: "Página de busca de conteúdo",
+				path: "/search",
+				element: <Search />,
+			}
+		];
+	}
 }
 
 export const RoutesInfo = new routesInfo();
